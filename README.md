defd6f511982b834e81a8d1298e7b5562b6054f6
Bitora <219180682+laysar@users.noreply.github.com>
2026-09-15 15:25:12 +0300

    Release 1.5.0

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
 ```
 implementation("io.github.laysar:G2Background:1.0.0")
 ```
+=======
+>>>>>>> c502843 (Release 1.5.0)
---
+++ app/src/main/java/com/laysar/G2Background/MainActivityActivity.java
@@ -0,0 +1,29 @@
+package com.laysar.G2Background;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.laysar.G2.G2Background;
+
+public class MainActivityActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		EdgeToEdge.enable(this);
+		setContentView(R.layout.main_activity);
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+			Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+			return insets;
+		});
+		
+		G2Background lina = new G2Background(this);
+		
+	}
+}
